43b0a730733c3a6dd0cd2520285e4b4efe627125
Cem <cemhekimoglu.ch@gmail.com>
2018-04-12 16:45:17 +0000

    Refactor the function findWaldo to use the forEach()

--- callbackwaldo.js
+++ callbackwaldo.js
@@ -1,14 +1,23 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function positionFound(i) {
+//   console.log("Found Waldo at index " + i +"!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], positionFound);
+
+
+var findWaldo = ["Alice", "Bob", "Waldo", "Winston"]
+findWaldo.forEach(function(element, food) {
+  if(element === "Waldo") {
+  console.log("Found Waldo at index " + food +"!");
   }
-}
+\});
 
-function positionFound(i) {
-  console.log("Found Waldo at index " + i +"!");
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], positionFound);
